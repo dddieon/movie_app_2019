793323335ce8721108b835941385047772e89d3f
DDDieon <59174247+DDDieon@users.noreply.github.com>
2020-09-22 21:45:38 +0900

    Set Router and Sub-pages

--- src/App.js
+++ src/App.js
@@ -1,54 +1,18 @@
 import React from "react"
-import axios from "axios"
-import Movie from "./Movie"
-import "./App.css"
+import { HashRouter, Route } from "react-router-dom"
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Detail from "./routes/Detail"
+import Navigation from "./components/Navigation"
 
-class App extends React.Component {
-    state = {
-        isLoading: true,
-        movies: [], // state인 movies
-    }
-    getMovies = async () => {
-        const {
-            data: {
-                data: { movies },
-            },
-        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
-        this.setState({ movies: movies, isLoading: false }) //state인 movies를 axios의 movies로 재정의
-    }
-    componentDidMount() {
-        this.getMovies()
-    }
-
-    render() {
-        const { isLoading, movies } = this.state
-        return (
-            <section className="container">
-                {isLoading ? (
-                    <div className="loader">
-                        <span className="loader__text">Loading...</span>
-                    </div>
-                ) : (
-                    <div className="movies">
-                        {movies.map((movie) => {
-                            return (
-                                //컴포넌트는 HTML을 반환한다
-                                <Movie
-                                    key={movie.id}
-                                    id={movie.id}
-                                    year={movie.year}
-                                    title={movie.title}
-                                    summary={movie.summary}
-                                    poster={movie.medium_cover_image}
-                                    genres={movie.genres}
-                                ></Movie>
-                            )
-                        })}
-                    </div>
-                )}
-            </section>
-        )
-    }
+function App() {
+    return (
+        <HashRouter>
+            <Navigation></Navigation>
+            <Route path="/" exact={true} component={Home}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route path="/movie/:id" component={Detail}></Route>
+        </HashRouter>
+    )
 }
-
 export default App
